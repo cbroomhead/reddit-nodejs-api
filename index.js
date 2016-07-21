13b8e1b332cf745691ecd658aa3e2e3a1a424c2c
Celine Broomhead <celine.broomhead@gmail.com>
2016-07-21 14:21:18 +0000

    Adding the index.js with all my test functions. Not crucial functionality in the file. Also allows you to create a user programmatically.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 // create a connection to our Cloud9 server
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'ziad_saab', // CHANGE THIS :)
+  user     : 'cbroomhead', 
   password : '',
   database: 'reddit'
 });
@@ -13,26 +13,120 @@ var connection = mysql.createConnection({
 var reddit = require('./reddit');
 var redditAPI = reddit(connection);
 
-// It's request time!
-redditAPI.createUser({
-  username: 'hello23',
-  password: 'xxx'
-}, function(err, user) {
-  if (err) {
-    console.log(err);
-  }
-  else {
-    redditAPI.createPost({
-      title: 'hi reddit!',
-      url: 'https://www.reddit.com',
-      userId: user.id
-    }, function(err, post) {
-      if (err) {
-        console.log(err);
-      }
-      else {
-        console.log(post);
-      }
-    });
-  }
-});
+
+//It's request time!
+// function userCreatesPost() {
+
+// redditAPI.createUser({
+//   username: 'wildOperation',
+//   password: 'secret4'
+// }, function(err, user) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   else {
+//     //console.log(user)
+//     redditAPI.createPost({
+//       title: 'I hate google',
+//       url: 'https://www.googlesucks.com',
+//       userId: user.id
+//     }, function(err, post) {
+//       if (err) {
+//         console.log(err);
+//       }
+//       else {
+//         console.log(post);
+//       }
+//     });
+//   }
+// });
+// }
+
+// userCreatesPost();
+
+
+//function(options, callback)
+// redditAPI.getAllPosts (function(err, post) {
+//       if (err) {
+//         console.log(err);
+//       }
+//       else {
+//         console.log(post);
+//       }
+// });
+
+
+//getAllPostsForUser: function(userId, options, callback) 
+// redditAPI.getAllPostsForUser(16, function(err, post) {
+//   if(err){
+//     console.log(err);
+//   }
+//   else {
+//     console.log(post);
+//   }
+// });
+
+//
+
+
+//getSinglePost: function (postId, callback)
+// redditAPI.getSinglePost( 38, function(err, post){
+//   if(err){
+//     console.log(err);
+//   }
+//   else {
+//     console.log(post);
+//   }
+// })
+
+// redditAPI.createSubreddit ('worldnews', function(err, post){
+//   if (err){
+//     console.log(err);
+//   }
+//   else {
+//     console.log(post);
+//   }
+// })
+
+// redditAPI.createSubreddit({
+//       name: 'worldnews',
+//       description: 'read about horrible shit happening',
+//     }, function(err, post) {
+//       if (err) {
+//         console.log(err);
+//       }
+//       else {
+//         console.log(post);
+//       }
+//     });
+
+
+// redditAPI.getAllSubreddits (function (err, post){
+//   if(err){
+//     console.log(err);
+//   }
+//   else{
+//     console.log(post);
+//   }
+// })
+
+// redditAPI.createPost({
+//   title: 'My cat is the cutest',
+//   url: 'imgur.cutecat.com',
+//   userId: 16
+// }, 4, function(err, post) {
+//       if (err) {
+//         console.log(err);
+//       }
+//       else {
+//         console.log(post);
+//       }
+//     });
+
+
+
+
+
+
+
+
