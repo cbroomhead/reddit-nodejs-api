692945f5a77e041042c851e356b3823e6dda6bcb
Celine Broomhead <celine.broomhead@gmail.com>
2016-07-25 20:36:53 +0000

    Adding Ex4.

--- webserver.js
+++ webserver.js
@@ -1,12 +1,26 @@
+
+// load the mysql library
+var mysql = require('mysql');
+
+// create a connection to our Cloud9 server
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'cbroomhead', 
+  password : '',
+  database: 'reddit'
+});
+
+// load our API and pass it the connection
+var reddit = require('./reddit');
+var redditAPI = reddit(connection);
+
+////////////
+
 var express = require('express');
 var app = express();
 
-//https://reddit-nodejs-api-cbroomhead.c9users.io/hello?name=John
-/*app.get('/user/:id', function(req, res) {
-  res.send('user ' + req.params.id);
-});*/
 
-app.get('/calculator/:operation/:num1/:num2', function (req, res) {
+/*app.get('/calculator/:operation/:num1/:num2', function (req, res) {
     console.log(req.params, "THESE ARE THE PARAMS");
     //res.send(req.params)
     if(req.params.operation === 'add'){
@@ -28,7 +42,63 @@ app.get('/calculator/:operation/:num1/:num2', function (req, res) {
     else{
         res.sendStatus(400);
     }
-});
+});*/
+
+app.get('/posts', function (req, res) {
+    //#1 call function getAllPosts /
+    //#2 create funciton callback/
+    //#3 if error, return status code /
+    //#4 if no error, show all posts in html
+    //#5 format in html (console.log always to see results format)
+    
+   redditAPI.getAllPosts ('controversial', function(err, posts) {
+      if (err) {
+        console.log(err);
+        res.sendStatus(403);
+      }
+      else {
+       
+       function createLi(post){
+        return `
+        <li>
+          <h2> Post Title: ${post.title} </h2>
+          <a href="${post.url}">Go to url ${post.url}</a>
+          <p>Post userId: ${post.user.username}</p>
+          <p>Post created at: ${post.user.createdAt}</p>
+        </li>
+        
+       
+        
+        `;  
+       }
+       
+        var html = `
+        <div id="contents">
+          <h1>List of contents</h1>
+            <ul class="contents-list">
+             ${posts.map(function(post){
+               return createLi(post);
+             }).join("")}
+            </ul>
+        </div>
+        `;        
+        
+        
+        res.send(html);
+        
+      }
+})
+})
+
+
+    
+
+
+
+
+
+
+
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
