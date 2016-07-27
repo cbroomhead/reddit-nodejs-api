bc404d63072c38b538a7c3c1c5447f8b0959aeaf
Celine Broomhead <celine.broomhead@gmail.com>
2016-07-27 15:34:11 +0000

    Adding the index.js webserver file. In conjuction with the reddit.js push, this file allows a new user to go to the signup page and then create a username and password. Once those are created successfully, the user is redirected to a login page from which he can login. On the login page, the correct functions are called to stored and grad the token and the userIds to establish a session for the user to start creating post. Once the user is logged in successfully, he/she is redirected to a Create Post page where a new post title and url can be submitted. Once the post is submitted, it is stored in the appropriate posts table in the database to be viewed and used by another page not yet written. Once the post is sucessfully created, the user is redirected to a simple page where a success messsage is displayed.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-// load the mysql library
+// External modules
 var mysql = require('mysql');
+var express = require('express');
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser');
+
+//User modules
+var reddit = require('./reddit');
+
+// load our API and pass it the connection
+
 
 // create a connection to our Cloud9 server
 var connection = mysql.createConnection({
@@ -9,14 +18,197 @@ var connection = mysql.createConnection({
   database: 'reddit'
 });
 
-// load our API and pass it the connection
-var reddit = require('./reddit');
 var redditAPI = reddit(connection);
 
+//Create express app
+var app = express();
 
-//It's request time!
-// function userCreatesPost() {
+//Middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+  }));
+  
+app.use(cookieParser())
 
+function checkLoginToken(request, response, next) {
+  if (request.cookies.SESSION) {
+    redditAPI.getUserFromSession(request.cookies.SESSION, function(err, user) {
+      if (err) {
+        console.log(err.stack)
+      }
+      else {
+        if (user) {
+          //console.log("we get inside this else from checkLoginToken")
+          request.loggedInUser = user;
+        }
+        next();
+      }
+    });
+  }
+  else {
+    next();
+  }
+}
+
+app.use(checkLoginToken);
+  
+
+
+//Routes
+
+/*****************************/
+//SIGNUP 
+app.get('/signup', function (req, res) {
+  
+  //console.log(res);
+  
+        var signupform = `
+  <form action="/signup" method="POST"> 
+  <h1> This is where you SIGN-UP </h1>
+  <div>
+    <input type="text" name="username" placeholder="Enter a username">
+  </div>
+  <div>
+    <input type="password" name="password" placeholder="Enter a Password">
+  </div>
+  <button type="submit">Sign Up!</button>
+</form>`;
+  
+  res.send(signupform);
+})
+
+app.post('/signup', function(req, res) {
+
+  redditAPI.createUser({
+    username: req.body.username,
+    password: req.body.password
+  }, function(err, post) {
+
+    if (err) {
+      res.status(500).send("try again later1");
+      console.log(err.stack);
+    }
+
+    else {
+        //console.log("You get into the right else.");
+        //res.send("We are creating your username and password.")
+        res.redirect(`/login`);
+    }
+  })
+})
+
+
+//LOGIN
+app.get('/login', function (req, res) {
+  
+  var loginform = `
+  <form action="/login" method="POST"> 
+  <h1> This is where you LOGIN </h1>
+  <div>
+    <input type="text" name="username" placeholder="Enter a username">
+  </div>
+  <div>
+    <input type="password" name="password" placeholder="Enter a Password">
+  </div>
+  <button type="submit">Login Up!</button>
+</form>`;
+  
+  res.send(loginform);
+})
+
+app.post('/login', function(req, res) {
+
+  redditAPI.checkLogin(req.body.username, req.body.password, function(err, login) {
+    if (err) {
+      console.log('please' + err.stack);
+    }
+    else {
+     // console.log("This is the login:" + login.username + "user Id:" + login.id); //at thi spoint, the login is true. 
+      //res.redirect(`/homepage`);
+
+      redditAPI.createSession(login.id, function(err, token) {
+        if (err) {
+          res.status(500).send('an error occurred. please try again later!');
+        }
+        else {
+          console.log(login.username);
+          res.cookie('SESSION', token); // the secret token is now in the user's cookies!
+          res.redirect('/createPost');
+        }
+      });
+    }
+  })
+
+})
+
+//CREATE POST
+app.get('/createPost', function (req, res) {
+  var posthtml = `
+<form action="/createPost" method="POST"> 
+  <h1> CREATE A POST </h1>
+  <div>
+    <input type="Post Title" name="posttitle" placeholder="Post Title">
+  </div>
+   <div>
+    <input type="Post URL" name="posturl" placeholder="Post Url">
+  </div>
+  <button type="submit">Submit Post</button>
+</form>
+  `
+  res.send(posthtml);
+  
+})
+
+app.post('/createPost', function(request, response) {
+  if (!request.loggedInUser) {
+    // HTTP status code 401 means Unauthorized
+    response.status(401).send('You must be logged in to create content!');
+  }
+  else {
+    console.log("the createpost post work");
+    redditAPI.createPost({
+      title: request.body.posttitle,
+      url: request.body.posturl,
+    }, request.loggedInUser.id, function(err, post) {
+      if(err){
+        console.log(err)  
+      }
+      else {
+        var postpage=`
+  <h1>You've submitted your post!!</h1>
+  <h2> Good luck with collecting votes! </h2>
+  `
+  response.send(postpage);
+      
+      }
+    })
+  }
+})
+
+//GO TO POST
+// app.get('/post', function(request, response){
+//   var postpage=`
+//   <h1>You've displayed your </h1>
+//   `
+  
+// })
+
+
+
+
+
+/* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
+
+// Boilerplate code to start up the web server
+var server = app.listen(process.env.PORT, process.env.IP, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+
+//function userCreatesPost() {
 // redditAPI.createUser({
 //   username: 'wildOperation',
 //   password: 'secret4'
@@ -25,33 +217,35 @@ var redditAPI = reddit(connection);
 //     console.log(err);
 //   }
 //   else {
-//     //console.log(user)
+    //console.log(user)
 //     redditAPI.createPost({
-//       title: 'I hate google',
-//       url: 'https://www.googlesucks.com',
+//       title: 'Chocolate is my favorite',
+//       url: 'www.seescandy.com  ',
 //       userId: user.id
-//     }, function(err, post) {
+//     }, 4, function(err, post) {
 //       if (err) {
 //         console.log(err);
 //       }
+      
 //       else {
 //         console.log(post);
 //       }
 //     });
-//   }
-// });
-// }
+//   //}
+// //});
+// //}
 
 // userCreatesPost();
 
 
 //function(options, callback)
-// redditAPI.getAllPosts (function(err, post) {
+// redditAPI.getAllPosts ('controversial', function(err, post) {
 //       if (err) {
 //         console.log(err);
 //       }
 //       else {
 //         console.log(post);
+//         connection.end();
 //       }
 // });
 
@@ -123,6 +317,58 @@ var redditAPI = reddit(connection);
 //       }
 //     });
 
+
+// redditAPI.createComment ({
+//   body: "look at the katana",
+//   postId: 40,
+//   parentId: 16,
+//   userId: 6
+//   }, function (err, comment){
+//     if(err){
+//       console.log(err);
+//     }
+//     else {
+//       console.log(comment);
+//     }
+//   })
+
+// redditAPI.getCommentsForPost (40, function (err, arraycomments){
+//   if (err){
+//     console.log(err)
+//   }
+//   else {
+//     console.log(arraycomments) //this return an array
+//   }
+// })
+
+
+
+// var voteObj ={
+//     postId: 40,
+//     userId : 9,
+//     vote : 1
+// }
+//This function will take a vote object with postId, userId, vote.
+// redditAPI.createOrUpdateVote (voteObj, function (err, result){
+//   if (err){
+//     console.log(err)
+//   }
+//   else {
+//     console.log(result) //this return an array
+//     connection.end();
+//   }
+// })
+
+
+
+//getAllPosts: function(options, sortingMethod, callback)
+
+
+
+
+
+
+//connection.end(); this will close the connection 
 
 
 
