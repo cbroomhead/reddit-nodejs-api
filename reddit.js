d79317799a159bf39b79e216260ae673cd321166
Celine Broomhead <celine.broomhead@gmail.com>
2016-08-05 18:20:54 +0000

    Document featuring all the backend functions to query mysql and callback to the front end.

--- reddit.js
+++ reddit.js
@@ -65,17 +65,15 @@ module.exports = function RedditAPI(conn) {
     },  
     createPost: function(post, subID, callback) {
       conn.query(
-        'INSERT INTO posts (userId, title, url, createdAt, subreddits_id) VALUES (?, ?, ?, ?, ?)', [post.userId, post.title, post.url, new Date(), subID],
+        'INSERT INTO posts ( title, url,userId, createdAt, subreddits_id) VALUES (?, ?, ?, ?, ?)', [ post.title, post.url, post.userId, new Date(), subID],
         
         function(err, result) {
           if (err) {
             callback(err);
           }
           else {
-            
             conn.query(
               'SELECT id, title, url, userId, createdAt, updatedAt, subreddits_id FROM posts WHERE id = ?', [result.insertId],
-              
               function(err, result) {
                 if (err) {
                   callback(err);
@@ -91,41 +89,42 @@ module.exports = function RedditAPI(conn) {
       );
     },
     getAllPosts: function(sortingMethod, options, callback) {
+      
       if (!callback) {
         callback = options;
         options = {};
       }
-    var groupedby;   
+    var groupedby = 'votescore'; // default
       if(sortingMethod === 'new'){
-        groupedby = 'p.createdAt DESC';
+        groupedby = `createdAt`;
       }
       if(sortingMethod === 'top'){
-        groupedby = 'votescore';
+        groupedby = `votescore`;
       }
       if(sortingMethod === 'hot'){
-        console.log("this is the hot post")
-        groupedby ='longestpost'
+        groupedby =`longestpost`;
       }
-      
-//Controversial ranking: = numUpvotes < numDownvotes ? totalVotes * (numUpvotes / numDownvotes) : totalVotes * (numDownvotes / numUpvotes)
       if (sortingMethod === 'controversial'){
-         groupedby= `
-         if((count(if(vote=1, 1, null))>count(if(vote=-1, 1, null))),(sum(vote) * count(if(vote=1, 1, null))) / count(if(vote=-1, 1, null)),null)`;
+         groupedby= `contra`;
       }
       
-      var limit = options.numPerPage || 25; // if options.numPerPage is "falsy" then use 25
+      var limit = options.numPerPage || 100; // if options.numPerPage is "falsy" then use 25
       var offset = (options.page || 0) * limit;
       conn.query(`
         SELECT 
-          username, u.id, p.title, p.url, p.createdAt , p.updatedAt, p.userId, sum(v.vote) as votescore , 
-          (now() - p.createdAt) as longestpost
+          p.id as postId, username, u.id, p.title, p.url, 
+          p.createdAt as createdAt , 
+          p.updatedAt, p.userId, 
+          sum(v.vote) as votescore , 
+          (now() - p.createdAt) as longestpost,
+          (if((count(if(vote=1, 1, null))>count(if(vote=-1, 1, null))),(sum(vote) * count(if(vote=1, 1, null))) / count(if(vote=-1, 1, null)),null)) as contra
         FROM 
           posts as p
         JOIN users AS u ON p.userId = u.id
         JOIN subreddits AS s ON p.subreddits_id = s.id
         LEFT JOIN votes AS v ON p.id = v.postId 
         GROUP BY p.id
-        ORDER BY ${groupedby} 
+        ORDER BY ${groupedby} DESC
         LIMIT ? OFFSET ?`, [limit, offset],
         function(err, results) {
           if (err) {
@@ -134,22 +133,23 @@ module.exports = function RedditAPI(conn) {
           else {
             var mappedResults = results.map(function(obj) {
               return {
-                id: obj.id,
+                id: obj.postId,
                 title: obj.title,
                 url: obj.url,
                 createdAt: obj.createdAt,
                 updatedAt: obj.updatedAt,
                 userId: obj.userId,
+                postId: obj.postId,
                 user: {
                   id: obj.id,
                   username: obj.username,
                   createdAt: obj.createdAt,
                   updatedAt: obj.updatedAt
                 },
-                voteScore: obj.votescore
+                votescore: obj.votescore
               }
+              
             })
-
             callback(null, mappedResults);
           }
         }
@@ -193,14 +193,13 @@ module.exports = function RedditAPI(conn) {
             SELECT id, title, url, userId
             FROM posts
             WHERE posts.id = ?
-            ORDER BY posts.createdAt DESC
             `, [postId],
           function(err, results) {
             if (err) {
               callback(err);
             }
             else {
-              callback(null, results);
+              callback(null, results[0]);
             }
           }
         );
@@ -242,17 +241,6 @@ module.exports = function RedditAPI(conn) {
       
     },
     createComment: function(comment, callback) {
-      /*
-      if(!comment.UserId || !comment.postId ){
-        call(null, new Error ('user and postsId required'));
-        return;
-      }
-      if(!comment.parentId){
-        comment.parentId = null;
-      }
-      
-      */
-      
       conn.query(
         `INSERT INTO comments 
           (body, postId, parentId, userId) 
@@ -264,7 +252,6 @@ module.exports = function RedditAPI(conn) {
             console.log(err)
           }
           else {
-            console.log('I get inside the else statement');
             conn.query(
               `SELECT 
                 id, body, userId 
@@ -281,9 +268,9 @@ module.exports = function RedditAPI(conn) {
           }
         })
     },
-    /* getCommentsForPost: function(postId, callback) {
-        conn.query(
-          `
+    getCommentsForPost: function(postId, callback) {
+      conn.query(
+        `
         SELECT 
           c1.id as c1_id, c1.body as c1_body, c1.parentId as c1_parentId, 
           c2.id as c2_id, c2.body as c2_body, c2.parentId as c2_parentId,
@@ -293,93 +280,78 @@ module.exports = function RedditAPI(conn) {
         LEFT JOIN comments c3 ON c2.id = c3.parentId
         WHERE c1.parentId IS NULL AND c1.postId = ?
         `, [postId],
-          function(err, allcomments) {
-            if (err) {
-              //callback(err);
-              console.log(JSON.stringify(comments, null, 4));
-            }
-            else {
-              // var mappedAllComments = allcomments.map (function (obj){
-              //   return {
-              //     id: obj.id,
-              //     body: obj.body,
-              //     //createdAt: obj.createdAt,
-              //     //updatedAt: obj.updatedAt,
-              //     replies: []
-              //   }
-              // })
-
-              var finalCommentsArray = [];
-              var coomentsIndex = {};
-
-              allcomments.forEach(function(item) {
-                  var comment1;
-                  if (commentsIndex[item.c1_id]) {
-                    comment1 = commentsIndex[item.c1_id]
-                  }
-                  else {
-                    var comment1 = {
-                      id: item.c1_id,
-                      body: item.c1_body,
-                      parentId: item.c1_parentId,
-                      replies: []
-                    };
-                    //put the commentin theindes by it's id
-                    commentsIndex[commentGroup.c1_id] = comment1;
-                    //put it in the final comments araay 
-                    finalComments.push(comment1)
-                  }
-
-                  //there is an if statement here. 
-
-                  var comment2;
-                  if (commentsIndex[item.c2_id]) {
-                    comment1 = commentsIndex[item.c1_id]
-                  }
-                  else {
-                    var comment2 = {
-                      id: item.c2_id,
-                      body: item.c2_body,
-                      parentId: item.c2_parentId,
-                      replies: []
-                    };
-                    //put the commentin theindes by it's id
-                    commentsIndex[commentGroup.c1_id] = comment2;
-                    //put it in the final comments araay 
-                    comment1.replies.push(comment2)
-                  }
-
-                  if (item.c3_id !== null) {
-
-                    comment2.replies.push( = {
-                      id: item.c3_id,
-                      body: item.c3_body,
-                      parentId: item.c3_parentId,
-                      replies: []
-                    });
-                  }
-
-                }
-                console.log(comment1, comment2, comment3); console.log("-------------");
-
-              });
-
-
+        function(err, allcomments) {
+          if (err) {
+            console.log("not working");
           }
+          else {   
+            
+            var finalCommentsArray = []; //creating the final array in which we are going to push shit in
+            var commentsIndex = {}; //creating the object we will use to reference the different objects inside the array created by the results of the query
 
-        })//there is a function here!!! **********DO NOT DELETE*/
-    createOrUpdateVote: function(vote, callback) {
-     
-      if (vote.vote === 1 || vote.vote === -1 || vote.vote === 0) {
-        console.log(vote);
-        
+            allcomments.forEach(function(item) { //begining to loop through the results of the query which is an array full of objects
+              var comment1; //creating a variable to set the value of the index inside the object in the event that the item c1_id does not yest exist
+              if (commentsIndex[item.c1_id]) { //checking to see if the current object has a c1_id property that does not yet exist in the object commentsIndex
+                comment1 = commentsIndex[item.c1_id] //if we get inside the if, we will set the comment1 variable to equal the object with item.c1_id inside the object commtsIndex
+              }
+              else {  //else, if the reference to object item-c1_id does not yet exist, we will create it. 
+                var comment1 = {   //creating the object with properties. this will be a parent object with potentinal children
+                  id: item.c1_id, //the id will be set the ide of the comment 
+                  body: item.c1_body, //the body will be set to the body of the comment
+                  parentId: item.c1_parentId, //this is the id of the parent which will always be null in this case. 
+                  replies: [],
+                  //the object may potentially place other obejct inside this array
+                };
+                //put the commentin theindes by it's id
+                commentsIndex[item.c1_id] = comment1; //the object with id item.c1_id will be assigned the newly created object above 
+                //put it in the final comments araay 
+                finalCommentsArray.push(comment1) //then, once the reference to object is added to the commentsIndex object, we puch it into the final Array
+              }
+
+              //there is an if statement here. 
+
+              var comment2; //creating a comment2 variable to later assign it to a reference of an object inside the commentIndex object
+              if (commentsIndex[item.c2_id]) { // checking to see if the id of object (at the current object in results from query) already exists in the commentsIndex object
+                comment2 = commentsIndex[item.c2_id] // if it does exist, setting the comment2 variable to the id of the already existing reference of object. basically setting the reference to have a parentId
+              }
+              else { //otherwise if the object does not exist, then let's create it
+                var comment2 = {  //creating object with follwoing property, same thing as the fist comment1
+                  id: item.c2_id,
+                  body: item.c2_body,
+                  parentId: item.c2_parentId,
+                  replies: []
+                };
+                //put the commentin theindes by it's id
+                commentsIndex[item.c2_id] = comment2; //placing newly created object at the id of the 
+                //put it in the final comments araay 
+                commentsIndex[comment2.parentId].replies.push(comment2); //this will push object comment2
+              }
+
+              if (item.c3_id !== null) { //if there is a third comment, just create an object 
+
+                commentsIndex[item.c3_parentId].replies.push({ //this will push the object located indeisde the commentIndex at item.c3_parentId inside the replies of of the comment
+                  id: item.c3_id,
+                  body: item.c3_body,
+                  parentId: item.c3_parentId
+                });
+              }
+            })
+          }
+         // console.log(finalCommentsArray);
+          callback(null, finalCommentsArray)
+        })
+    },
+    createOrUpdateVote: function(vote, user, callback) {
+      //console.log(user);
+      if (vote.vote === '1' || vote.vote === '-1' || vote.vote === '0') {
         conn.query(
-          'INSERT INTO `votes` SET `postId`= ? , `userId`= ?, `vote`= ?, `createdAt`= ? ON DUPLICATE KEY UPDATE `vote`= ?;',[vote.postId , vote.userId, vote.vote, new Date(), vote.vote],
+          'INSERT INTO `votes` SET `postId`= ? , `userId`= ?, `vote`= ?, `createdAt`= ? ON DUPLICATE KEY UPDATE `vote`= ?;',[vote.postId , user, vote.vote, new Date(), vote.vote],
           function(err, voice) {
             if (err) {
               callback(err);
             }
             else {
+              //console.log(voice)
               callback(null, voice);
             }
           })
@@ -407,6 +379,7 @@ module.exports = function RedditAPI(conn) {
                 
                   if(res.length === 0){
                     console.log("there was an err, please try again. Redirect to login.")
+                    callback('no match')
                   }
                   else {
                    // console.log(res[0])
@@ -414,9 +387,10 @@ module.exports = function RedditAPI(conn) {
                       bcrypt.compare(password, res[0].password, function(err, res) {
                         if(err){
                           console.log(err)
+                          callback(err)
                         }
                         else{
-                          console.log(user)
+                          //console.log(user)
                           callback(null, user)
                         }
                       }) 
@@ -438,7 +412,6 @@ module.exports = function RedditAPI(conn) {
   }) 
     },
     getUserFromSession: function (token, callback){
-      console.log(token);
       conn.query(`
         SELECT 
           token , userId
@@ -456,21 +429,41 @@ module.exports = function RedditAPI(conn) {
             callback(null, res);
           }
         }
-    )}
- 
-    
-    
-    
-
-    
+    )},
+    deleteSession: function(token, callback){
+     // console.log("this is the:" + token);
+      conn.query(`DELETE
+        FROM sessions
+        WHERE token = ?`, [token], function (err, res) {
+          if(err){
+            callback(err);
+          }
+          else{
+            console.log("go check to see if the token is still in the sessions table")
+           callback();
+            
+          }
+          
+        })
+    },
+    getVotesForPost: function (postId, callback){
+      conn.query (
+        `SELECT 
+          sum(vote) as votescore
+        FROM 
+          votes
+        WHERE postId= ?  
+        `, [postId]
+        , function (err, res){
+          if(err){
+            console.log(err);
+          }
+        else {
+          callback(null, res[0].votescore);
+        }
+      })
+    }
   }
 }
-/*
-SELECT token
-FROM sessions
-LEFT JOIN users
-ON sessions.userId = users.id
 
-table1.column_name=table2.column_name;
-*/
     
